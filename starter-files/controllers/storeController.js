e1abd602b9042a3137b2cea5f06e9c750af6c487
Bryce Hipp <bhipp@qetechnology.com>
2018-01-27 22:49:56 -0600

    Add functionality to edit store and specify address

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -5,6 +5,12 @@ exports.homePage = (req, res) => {
   res.render('index', { title: 'Home' });
 };
 
+exports.getStores = async (req, res) => {
+  const stores = await Store.find();
+
+  res.render('stores', { title: 'Stores', stores });
+};
+
 exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add Store' });
 };
@@ -17,11 +23,19 @@ exports.createStore = async (req, res) => {
   res.redirect(`/store/${store.slug}`);
 };
 
-exports.getStores = async (req, res) => {
-  const stores = await Store.find();
+exports.editStore = async (req, res) => {
+  const store = await Store.findOne({ _id: req.params.id });
 
-  res.render('stores', {
-    title: 'Stores',
-    stores,
+  res.render('editStore', { title: `Edit ${store.name}`, store });
+};
+
+exports.updateStore = async (req, res) => {
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true, // Return the new store instead of the old one
+    runValidators: true,
   });
+
+  req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store</a>`);
+
+  res.redirect(`/stores/${store._id}/edit`);
 };
